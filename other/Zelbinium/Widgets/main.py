847ad3aca9a37cea340b598dfece016863b73c04
csimon <epeios-q37@users.noreply.github.com>
2023-09-07 15:19:06 +0200

    07/09/2023 15:18:28

--- other/Zelbinium/Widgets/main.py
+++ other/Zelbinium/Widgets/main.py
@@ -1,32 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2018 Claude SIMON (https://q37.info/s/rmnmqd49)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
-import os, sys
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append("../../atlastk")
-
 import atlastk, html, re
 
 """
@@ -40,10 +11,8 @@ def clean(s):
 
 
 def displayCode(dom, element):
-# source = dom.nextSibling(dom.firstChild(element));
   source = dom.executeString(f"getOrGenerateId(getElement('{element}').firstElementChild.nextElementSibling);")
   code = clean(dom.getValue(source))
-  # target = dom.nextSibling(dom.firstChild(dom.nextSibling(dom.firstChild(dom.nextSibling(dom.nextSibling(source))))))
   target = dom.executeString(f"getOrGenerateId(getElement('{source}').nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling);")
   dom.setValue(target, html.escape(code))
 
