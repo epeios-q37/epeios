dd4933bddfb5d86a0a9de5ff12a2b3fff30cac45
csimon <epeios-q37@users.noreply.github.com>
2025-05-10 19:49:55 +0200

    10/05/2025 19:49:55

--- other/BPY/Apps/UCUq/Boilerplate/boilerplate.py
+++ other/BPY/Apps/UCUq/Boilerplate/boilerplate.py
@@ -1,11 +1,8 @@
 import ucuq, atlastk
 
 async def atk(dom):
-  global pin, level
-  
-  pin, level = ucuq.getKitHardware(await ucuq.ATKConnectAwait(dom, BODY))["OnBoardLed"]
-
-  ucuq.GPIO(pin).high(not(level))
+  await ucuq.ATKConnectAwait(dom, BODY)
+  ucuq.GPIO(2).low()
   
 async def atkSwitch(dom, id):
-  ucuq.GPIO(pin).high( (await dom.getValue(id) == "true") == level)
+  ucuq.GPIO(2).high(await dom.getValue(id) == "true")
