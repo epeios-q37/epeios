c5ac85795d3292cceee1237fccea556220ff4b2a
csimon <epeios-q37@users.noreply.github.com>
2025-10-04 19:50:29 +0200

    04/10/2025 19:50:29

--- other/BPY/Apps/UCUq/EPaper/epaper.py
+++ other/BPY/Apps/UCUq/EPaper/epaper.py
@@ -10,21 +10,38 @@ W_TXT_X = "TXTX"
 W_TXT_Y = "TXTY"
 W_TXT_FONT_SIZE = "TXTFontSize"
 W_TXT_FONT = "TXTFont"
+W_TXT_CENTER_X = "TXTCenterX"
+W_TXT_CENTER_Y = "TXTCenterY"
+W_TXT_CANVAS = "TXTCanvas"
 
 async def txtUpdate(dom):
-  text, x, y, width, font, fontSize = await dom.getValues((W_TXT_TEXT, W_TXT_X, W_TXT_Y, W_TXT_WIDTH, W_TXT_FONT, W_TXT_FONT_SIZE)).values()
+  text, x, y, width, font, fontSize, centerX, centerY = (await dom.getValues((W_TXT_TEXT, W_TXT_X, W_TXT_Y, W_TXT_WIDTH, W_TXT_FONT, W_TXT_FONT_SIZE, W_TXT_CENTER_X, W_TXT_CENTER_Y))).values()
 
-  dom.executeVoid(f"createTextCanvas('{text}', {x}, {y}, {width}, \"{font}\", {fontSize})")
+  coords = json.loads(await dom.executeString(f"JSON.stringify(createTextCanvas('{W_TXT_CANVAS}','{text}', {x}, {y}, {width}, \"{font}\", {fontSize}, {centerX}, {centerY}));"))
+
+  values = {W_TXT_X: coords['x'], W_TXT_Y: coords['y']}
+
+  if centerX != "true":
+    del values[W_TXT_X]
+    
+  if centerY != "true":
+    del values[W_TXT_Y]
+
+  if values:
+    await dom.setValues(values)
 
 
 async def atk(dom):
   global epaper
 
   await ucuq.ATKConnectAwait(dom, BODY)
+
   epaper = ucuq.SSD1680_SPI(7, 1, 2, 3, ucuq.SPI(1, baudrate=2000000, polarity=0, phase=0, sck=4, mosi=6))
   epaper.fill(0).hText("E-paper ready !", 50).hText(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 75).show()
+
   await dom.focus(W_QRC_TEXT)
   await txtUpdate(dom)
+
 
 # Called from JS!
 async def atkDisplay(dom, data):
@@ -48,13 +65,29 @@ async def atkTXTUpdate(dom):
 
 
 async def atkTXTSubmit(dom):
-  text, x, y, width, font, fontSize = await dom.getValues((W_TXT_TEXT, W_TXT_X, W_TXT_Y, W_TXT_WIDTH, W_TXT_FONT, W_TXT_FONT_SIZE)).values()
+  text, x, y, width, font, fontSize, centerX, centerY = (await dom.getValues((W_TXT_TEXT, W_TXT_X, W_TXT_Y, W_TXT_WIDTH, W_TXT_FONT, W_TXT_FONT_SIZE, W_TXT_CENTER_X, W_TXT_CENTER_Y))).values()
 
-  dom.executeVoid(f"displayText('{text}', {x}, {y}, {width}, \"{font}\", {fontSize})")
+  await dom.executeVoid(f"displayText('{W_TXT_CANVAS}','{text}', {x}, {y}, {width}, \"{font}\", {fontSize}, {centerX}, {centerY})")
 
 
 async def atkTXTClear(dom):
   await dom.setValue(W_TXT_TEXT, "")
+
+
+async def atkTXTCenterX(dom, id):
+  if ( ( await dom.getValue(id) ) == "true"):
+    await dom.disableElement(W_TXT_X)
+    await txtUpdate(dom)
+  else:
+    await dom.enableElement(W_TXT_X)
+
+
+async def atkTXTCenterY(dom, id):
+  if ( ( await dom.getValue(id) ) == "true"):
+    await dom.disableElement(W_TXT_Y)
+    await txtUpdate(dom)
+  else:
+    await dom.enableElement(W_TXT_Y)
 
 
 async def atkReset(dom, id):
